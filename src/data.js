dd563b729043e1cf82fc01816823858b707e02e8
kubusjestsuper <58520735+kubusjestsuper@users.noreply.github.com>
2020-06-04 21:19:18 +0200

    dziala kurwa

---
+++ src/data.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CoronaData = ({
+	today,
+	active,
+	critical,
+	recovered,
+	total,
+	deaths,
+	date,
+}) => {
+	return <>data: {date}</>;
+};
+
+export default CoronaData;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import CoronaData from './data';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+	var unirest = require('unirest');
+	var req = unirest('GET', 'https://covid-193.p.rapidapi.com/statistics');
+
+	req.query({
+		country: 'poland',
+	});
+	req.headers({
+		'x-rapidapi-host': 'covid-193.p.rapidapi.com',
+		'x-rapidapi-key': '81fef1d965mshef8014d73e013e7p1933dcjsn07170ff4593b',
+		useQueryString: true,
+	});
+	const [todayCases, setTodayCases] = useState('');
+	const [activeCases, setActiveCases] = useState(0);
+	const [criticalCases, setCriticalCases] = useState(0);
+	const [recoveredCases, setRecoveredCases] = useState(0);
+	const [totalCases, setTotalCases] = useState(0);
+	const [totalDeaths, setTotalDeaths] = useState(0);
+	const [date, setDate] = useState('');
+	req.end(function (res) {
+		if (res.error) throw new Error(res.error);
+
+		let [data] = res.body.response;
+		setTodayCases(data.cases.new);
+		setActiveCases(data.cases.active);
+		setCriticalCases(data.cases.critical);
+		setRecoveredCases(data.cases.recovered);
+		setTotalCases(data.cases.total);
+		setTotalDeaths(data.deaths.total);
+		setDate(data.day);
+	});
+
+	return (
+		<>
+			<CoronaData
+				today={todayCases}
+				active={activeCases}
+				critical={criticalCases}
+				recovered={recoveredCases}
+				total={totalCases}
+				deaths={totalDeaths}
+				date={date}
+			/>
+		</>
+	);
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
